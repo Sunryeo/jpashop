e5caf28c1d90f27f4ce1efbd2980dd91abeafcb5
Sunryeo <elma9700@gmail.com>
2023-01-28 20:10:47 +0900

    add: category & item 엔티티 매핑

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    // jpa 스펙상 만들어둔 기본 생성자, 실제로 호출될 일은 없음
+    protected Address() {}
+
+    // 생성할 때만 값을 집어넣을 수 있게, 값이 변하지 않도록 보존하기 위해 setter를 제공하지 않는 대신 생성자를 만들어줌
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
