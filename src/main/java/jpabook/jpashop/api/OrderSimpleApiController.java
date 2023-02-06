f388d73aab9f983188c70bd9f3b9f6501d70e2a8
Sunryeo <elma9700@gmail.com>
2023-02-07 00:05:56 +0900

    add: dto로 변환하여 주문 목록 조회 api 구현

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.service.OrderService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderService orderService;
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName(); // Lazy 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress(); // Lazy 초기화
+        }
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public Result ordersV2() {
+        List<SimpleOrderDto> orders = orderService.findOrders(new OrderSearch()).stream()
+                .map(el -> new SimpleOrderDto(el)) // == .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+
+        return new Result(orders);
+    }
+}
